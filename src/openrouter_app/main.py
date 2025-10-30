37f9b3602022e149789046811213b7e2926739c4
iker-ortiz0225 <iker.ortiz02@somo.eus>
2025-10-30 11:52:25 +0100

    Versión 2: Constructor con headers

--- src/openrouter_app/main.py
+++ src/openrouter_app/main.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 from openrouter_app.modules.openrouter_client import OpenRouterClient
 
+load_dotenv()
+
 def main():
-    client = OpenRouterClient()
+    api_key = os.getenv("OPENROUTER_API_KEY")
+    client = OpenRouterClient(api_key)
 
 if __name__ == "__main__":
     main()
