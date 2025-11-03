834cfb0f622d3e2e7785df5c86c1331bde1e301f
Jon Medina <jon.medina04@somo.eus>
2025-11-03 09:18:36 +0100

    v2=llamada a OpenrouterClient pasando apikey

--- src/openrouter_app/main.py
+++ src/openrouter_app/main.py
@@ -1,8 +1,11 @@
 from openrouter_app.modules.openrouter_client import OpenRouterClient
+from dotenv import load_dotenv
+import os
 
 def main():
-    client = OpenRouterClient()
-    print("Clase creada correctamente.")
+   api_key = os.getenv("OPENROUTER_API_KEY")
+    client = OpenRouterClient(api_key)
+    print("Cliente inicializado con API Key.")
 
 if __name__ == "__main__":
     main()
