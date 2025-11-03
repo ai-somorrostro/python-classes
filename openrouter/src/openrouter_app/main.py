11f9179e1adb699ce9f0e4029f7f576561831171
asier.perez <asier.perez05@somo.eus>
2025-11-03 08:56:18 +0100

    Clase con constructor

--- openrouter/src/openrouter_app/main.py
+++ openrouter/src/openrouter_app/main.py
@@ -1,5 +1,12 @@
 from openrouter_app.modules.openrouter_client import OpenRouterClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 if __name__ == "__main__":
-    client = OpenRouterClient()
-    print("Clase OpenRouterClient creada correctamente.")
+    api_key = os.getenv("OPENROUTER_API_KEY")  # Tomamos la API key del archivo .env
+    client = OpenRouterClient(api_key)
+    print("Cliente inicializado con éxito:")
+    print("Base URL:", client.base_url)
+    print("Headers:", client.headers)
--- openrouter/src/openrouter_app/modules/openrouter_client.py
+++ openrouter/src/openrouter_app/modules/openrouter_client.py
@@ -1,2 +1,8 @@
 class OpenRouterClient:
-    pass
+    def __init__(self,api_key: str):
+        self.api_key = api_key
+        self.base_url = "https://openrouter.ai/api/v1/chat/completions"
+        self.headers = {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json"
+        }
