faf060d05c2f7228626bc272996181acdbf710f5
haimar.alvarez <haimar.alvarez05@somo.eus>
2025-11-03 09:25:53 +0100

    Funcionalidad LLM

--- openrouter/src/openrouter_app/main.py
+++ openrouter/src/openrouter_app/main.py
@@ -7,6 +7,7 @@ load_dotenv()
 if __name__ == "__main__":
     api_key = os.getenv("OPENROUTER_API_KEY")  # Tomamos la API key del archivo .env
     client = OpenRouterClient(api_key)
-    print("Cliente inicializado con éxito:")
-    print("Base URL:", client.base_url)
-    print("Headers:", client.headers)
+    prompt=input("Escribe tu prompt: ")
+    response = client.LLM(prompt)
+    print("Respuesta del modelo:")
+    print(response)
--- openrouter/src/openrouter_app/modules/openrouter_client.py
+++ openrouter/src/openrouter_app/modules/openrouter_client.py
@@ -1,3 +1,5 @@
+import requests
+
 class OpenRouterClient:
     def __init__(self,api_key: str):
         self.api_key = api_key
@@ -6,3 +8,23 @@ class OpenRouterClient:
             "Authorization": f"Bearer {self.api_key}",
             "Content-Type": "application/json"
         }
+           
+    def LLM(self, prompt: str) -> str:
+        """
+        Envía un prompt al modelo google/gemini-2.0-flash-exp:free
+        y devuelve directamente la respuesta.
+        """
+        payload = {
+            "model":"meta-llama/llama-4-maverick:free",
+            "messages": [{"role": "user", "content": prompt}]
+        }
+
+        response = requests.post(self.base_url, headers=self.headers, json=payload)
+
+        if response.status_code != 200:
+            raise Exception(f"Error {response.status_code}: {response.text}")
+
+        data = response.json()
+        return data["choices"][0]["message"]["content"]
+        
+    
