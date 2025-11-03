7a7d3df6d2c2063086bbede44ce24d284801be27
Jon Medina <jon.medina04@somo.eus>
2025-11-03 09:01:40 +0100

    V1=main creada con llamada a openrouter_client

---
+++ src/openrouter_app/main.py
@@ -0,0 +1,8 @@
+from openrouter_app.modules.openrouter_client import OpenRouterClient
+
+def main():
+    client = OpenRouterClient()
+    print("Clase creada correctamente.")
+
+if __name__ == "__main__":
+    main()
