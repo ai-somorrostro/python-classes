e676d21f9549adc9a9ef1716a87528e92aa77284
asier.perez <asier.perez05@somo.eus>
2025-11-03 09:11:30 +0100

    Metodo razonador en clas y main

--- openrouter/src/openrouter_app/main.py
+++ openrouter/src/openrouter_app/main.py
@@ -7,6 +7,9 @@ load_dotenv()
 if __name__ == "__main__":
     api_key = os.getenv("OPENROUTER_API_KEY")  # Tomamos la API key del archivo .env
     client = OpenRouterClient(api_key)
-    print("Cliente inicializado con éxito:")
-    print("Base URL:", client.base_url)
-    print("Headers:", client.headers)
+    
+    user_prompt = input("Introduce el prompt: ")
+    system_prompt = "Eres un experto explicando conceptos complejos de manera simple."
+    
+    respuesta = client.razonador(user_prompt,system_prompt)
+    print("Respuesta del razonador:", respuesta)
--- openrouter/src/openrouter_app/modules/openrouter_client.py
+++ openrouter/src/openrouter_app/modules/openrouter_client.py
@@ -1,3 +1,5 @@
+import requests
+
 class OpenRouterClient:
     def __init__(self,api_key: str):
         self.api_key = api_key
@@ -6,3 +8,27 @@ class OpenRouterClient:
             "Authorization": f"Bearer {self.api_key}",
             "Content-Type": "application/json"
         }
+    
+    def razonador(self,user_message, system_prompt):
+        """Make a request with a system prompt to define AI behavior"""
+        base_url="https://openrouter.ai/api/v1/chat/completions"
+        data = {
+            "model": "openai/gpt-oss-20b:free",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": system_prompt
+                },
+                {
+                    "role": "user",
+                    "content": user_message
+                }
+            ]
+        }
+        response = requests.post(base_url, headers=self.headers, json=data)
+
+        if response.status_code == 200:
+            result = response.json()
+            return result["choices"][0]["message"]["content"]
+        else:
+            return f"Error: {response.status_code} - {response.text}"
