1878db2b4458da9339d775a54ad2bb9d1448d6f4
asier.perez <asier.perez05@somo.eus>
2025-11-03 08:42:20 +0100

    Clase vacia

---
+++ openrouter/src/openrouter_app/main.py
@@ -0,0 +1,5 @@
+from openrouter_app.modules.openrouter_client import OpenRouterClient
+
+if __name__ == "__main__":
+    client = OpenRouterClient()
+    print("Clase OpenRouterClient creada correctamente.")
