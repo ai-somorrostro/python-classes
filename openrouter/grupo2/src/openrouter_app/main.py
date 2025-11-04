f45f4d251eec9650534272f0e7994fcab62aaba5
aimar-sarachaga <aimar.sarachaga02@gmail.com>
2025-11-04 11:18:20 +0100

    add: main.py basico para version 1

---
+++ openrouter/grupo2/src/openrouter_app/main.py
@@ -0,0 +1,8 @@
+from modules.openrouter_client import OpenRouterClient
+
+def main():
+    client = OpenRouterClient()
+    print(f"Cliente creado: {client}")
+
+if __name__ == "__main__":
+    main()
