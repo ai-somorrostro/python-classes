0cd10cecfbbb7801cfae8b6be9b084a03c041a9b
aimar-sarachaga <aimar.sarachaga02@gmail.com>
2025-11-04 11:02:04 +0100

    clase con constructor

--- openrouter/src/openrouter_app/modules/openrouter_client.py
+++ openrouter/src/openrouter_app/modules/openrouter_client.py
@@ -1,2 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class OpenRouterClient:
-    pass
+    def __init__(self, api_key: str = None):
+        if api_key is None:
+            api_key = os.getenv('OPENROUTER_API_KEY')
+        if not api_key:
+            raise ValueError("Se requiere una API key válida.")
+        
+        self.base_url = "https://openrouter.ai/api/v1/chat/completions"
+        self.headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
+        }
