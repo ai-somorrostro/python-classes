f3ec74648b33dc9aaf2b1cf7019ef935e45c3100
clemente-rodriguez <clemente.rodriguez98@somo.eus>
2025-11-04 10:59:26 +0100

    carpeta api + carcasas de la parte api

---
+++ grupo5/src/api/__init__.py
@@ -0,0 +1,4 @@
+# src/api/__init__.py
+from llm_api import llm_api
+
+llm_api()
---
+++ grupo5/src/api/llm_api.py
@@ -0,0 +1,6 @@
+# src/api/llm_api.py
+def llm_api():
+    print("comiendo uno mangito, controlando la sona.")
+
+if __name__ == "__llm_api__":
+    llm_api()
