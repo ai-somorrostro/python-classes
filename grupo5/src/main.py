87649f3172ab3e308860f673d1e97acaae5b1899
Ibai Garcia <ibai.garcia02@somo.eus>
2025-11-04 10:14:10 +0000

    metodo de llamada a generar imagenes finalizada

--- grupo5/src/main.py
+++ grupo5/src/main.py
@@ -17,10 +17,19 @@ def main():
     # Creacion del cliente de OpenRouter
     cliente = Op_client(api_key)
 
+    # Apartado 1: Llamada LLM normal
     # Pedimos que rellene el prompt al usuario
     print("¿Qué quieres decirle a Gemini 2.0?")
     prompt = input()
     print(cliente.llamada_LLM_normal(prompt))
+
+    # Apartado 3: Generación de imagen
+    print("Dame una breve descripcion de la imagen que quieres generar con GPT-5 Image Mini?")
+    prompt_imagen = input()
+    cliente.generate_image_with_model(prompt_imagen)
+
+
+
     
     print("Programa finalizado.")
 
