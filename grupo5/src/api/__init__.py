4f834568d1d6da7457f5639c5896b56386e61a17
clemente-rodriguez <clemente.rodriguez98@somo.eus>
2025-11-11 11:45:21 +0100

    subida inicial de api con get y post funcionales

--- grupo5/src/api/__init__.py
+++ grupo5/src/api/__init__.py
@@ -1,4 +1,2 @@
 # src/api/__init__.py
-from llm_api import llm_api
-
-llm_api()
+from .llm_api import app
--- grupo5/src/api/llm_api.py
+++ grupo5/src/api/llm_api.py
@@ -1,6 +1,21 @@
 # src/api/llm_api.py
-def llm_api():
-    print("comiendo uno mangito, controlando la sona.")
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-if __name__ == "__llm_api__":
-    llm_api()
+app = FastAPI()
+
+# Define el modelo de entrada
+class TextoEntrada(BaseModel):
+    mensaje: str
+
+@app.get("/")
+async def root():
+    return {"message": "hola, caracola!"}
+
+@app.post("/enviar_mensaje_post")
+async def mensaje(entrada: TextoEntrada):
+    return {"message": f"hola, {entrada.mensaje}!"}
+
+@app.get("/enviar_mensaje_get")
+async def mensaje(entrada: str):
+    return {"message": f"hola, {entrada}!"}
