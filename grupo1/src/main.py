e4a2384b1afe3d7a80c066831575087df80ca6f0
asier.perez <asier.perez05@somo.eus>
2025-11-11 12:21:46 +0100

    Main finalizado

--- grupo1/src/main.py
+++ grupo1/src/main.py
@@ -43,26 +43,28 @@ if __name__ == "__main__":
 
     print("Imagen guardada como imagen_generada.png")"""
 
-    user_message=input("Prompt para el razonador: ")
+    #Modelo razonador
+    user_message=input("\nPrompt para el razonador: ")
     
     chat_payload = {
         "user_message": user_message,
         "system_prompt": "Eres un experto explicando conceptos complejos de manera simple."
     }
     response = requests.post(f"{API_URL}/razonador", json=chat_payload)
-    print("Razonador:", response.json())
+    print("\nRazonador:", response.json()["response"])
 
-    prompt_llm=input("Prompt para el LLM: ")
+    #Modelo LLM
+    prompt_llm=input("\nPrompt para el LLM: ")
 
     llm_payload = {"prompt": prompt_llm}
     response = requests.post(f"{API_URL}/llm", json=llm_payload)
-    print("LLM:", response.json())
+    print("\nLLM:", response.json()["response"])
     
-    image_llm=input("Prompt para el LLM: ")
+    #Modelo imagen
+    image_llm=input("\nPrompt para la imagen: ")
 
     image_payload = {"prompt": image_llm}
     response = requests.post(f"{API_URL}/image", json=image_payload)
-    print("Imagen URL:", response.json())
     
     b64_string = response.json()["image_url"]
     if b64_string.startswith("data:image"):
@@ -71,4 +73,4 @@ if __name__ == "__main__":
     with open("imagen_generada.png", "wb") as f:
         f.write(base64.b64decode(b64_string))
 
-    print("Imagen guardada como imagen_generada.png")
+    print("\nImagen guardada como imagen_generada.png")
